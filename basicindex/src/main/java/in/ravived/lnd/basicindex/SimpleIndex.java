52a1dca4eba6d0ee673f2cfdd168405ceb0ba878
Ravi Ved <rved76@gmail.com>
2016-04-08 23:10:46 +0530

    Psuedocode for basic indexing

---
+++ basicindex/src/main/java/in/ravived/lnd/basicindex/SimpleIndex.java
@@ -0,0 +1,62 @@
+package in.ravived.lnd.basicindex;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.List;
+
+public class SimpleIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	   //check if argument exist and its a directory
+		
+		//get list of files in a directory - no recursive feature
+		
+		// for every file in directory ...
+		
+		   //check if file  is of type txt
+		
+		  // open file and read every line
+		
+		  ///
+
+	}
+	
+	
+	private boolean checkArguments(String[] args){
+		return true;
+	}
+	
+
+	private List<File> getFileList(String dirPath){
+		return null;
+	}
+	
+
+	private void createIndexForEachFileInList(List<File> fileList){
+		
+		//For every file
+			//Open File in BufferReader mode
+	
+		
+				
+	}
+	
+	private void createIndexForEachOpenFile(BufferedReader openFileBR){
+		
+		// for every line in the file
+			// get every word in the file
+			
+			//createOrUpdateIndex()
+		
+		
+		
+	}
+	
+	private void createOrUpdateIndex(String word, String fileName){
+		
+	}
+
+	
+}
