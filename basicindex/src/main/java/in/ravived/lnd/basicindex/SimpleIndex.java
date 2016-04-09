6440372ead7eed31af52f4caa042a915674e3ab0
ravi <rved76@gmail.com>
2016-04-10 00:27:26 +0530

    updated option for enabling Sl4J tracing and options for switching to log4j

--- basicindex/src/main/java/in/ravived/lnd/basicindex/SimpleIndex.java
+++ basicindex/src/main/java/in/ravived/lnd/basicindex/SimpleIndex.java
@@ -7,73 +7,80 @@ import java.util.List;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class SimpleIndex {
 
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleIndex.class);
 
+	private static final Logger Logger = LoggerFactory.getLogger(SimpleIndex.class);
+
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-			LOGGER.info("Hello from BasicIndex..start");
-	   //check if argument exist and its a directory
-		
-		//get list of files in a directory - no recursive feature
-		
+		 
+		Logger.debug("BasicIndex..start");
+
+		SimpleIndex sindex = new SimpleIndex();
+
+		// check if argument exist and its a directory
+		if (sindex.checkArguments(args) == false) {
+			Logger.error("invalid arguments. usage .. SimpleIndex <path to index>");
+			return;
+		}
+
+		// get list of files in a directory - no recursive feature
+		sindex.getTxtFileList(args[0]);
+
 		// for every file in directory ...
-		
-		   //check if file  is of type txt
-		
-		  // open file and read every line
-		
-		  ///
+
+		// check if file is of type txt
+
+		// open file and read every line
+
+		///
 
 	}
-	
-	
-	private boolean checkArguments(String[] args){
-		
+
+	private boolean checkArguments(String[] args) {
+		Logger.info("" + args.length);
+		if (args.length > 0) {
+			Logger.info("argument value: " + args[0]);
+		}
+		if (args == null || args.length != 1) {
+			return false;
+		}
 		return true;
 	}
-	
 
-	private List<File> getFileList(String dirPath){
+	private List<File> getTxtFileList(String dirPath) {
 		File folder = new File(dirPath);
 		File[] listOfFiles = folder.listFiles();
-		for (File file : listOfFiles){
-		
-			FilenameUtils.getExtension(file.getName());
-			
-		
+		for (File file : listOfFiles) {
+
+			Logger.info(FilenameUtils.getExtension(file.getName()));
+
 		}
-		
+
 		return null;
 	}
-	
 
-	private void createIndexForEachFileInList(List<File> fileList){
-		
-		//For every file
-			//Open File in BufferReader mode
-	
-		
-				
+	private void createIndexForEachFileInList(List<File> fileList) {
+
+		// For every file
+		// Open File in BufferReader mode
+
 	}
-	
-	private void createIndexForEachOpenFile(BufferedReader openFileBR){
-		
+
+	private void createIndexForEachOpenFile(BufferedReader openFileBR) {
+
 		// for every line in the file
-			// get every word in the file
-			
-			//createOrUpdateIndex()
-		
-		
-		
-	}
-	
-	private void createOrUpdateIndex(String word, String fileName){
-		
+		// get every word in the file
+
+		// createOrUpdateIndex()
+
 	}
 
-	
+	private void createOrUpdateIndex(String word, String fileName) {
+
+	}
+
 }
